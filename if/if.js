c2c8851b8a4525da85211ab5c44e1aa15c4e7b63
Jean <jeanncavalcante@gmail.com>
2022-11-08 19:36:34 -0300

    first commit

---
+++ if/if.js
@@ -0,0 +1,19 @@
+let num = 8;
+
+if (num < 2) {
+  console.log("É menor q 2");
+} else if (num < 4) {
+  console.log("É menor q 2");
+} else if (num < 6) {
+  console.log("É menor q 6");
+} else {
+  console.log("e maioooor");
+}
+
+let nome = "Jean";
+
+if (nome == "jean" || nome == "Jean") {
+  console.log("Nome correto");
+} else {
+  console.log("Nome incorreto");
+}
---
+++ loops/while.js
@@ -0,0 +1,10 @@
+let nome = prompt("insira um nome");
+
+// while (nome != "jean") {
+//   alert("vc inseriu " + nome);
+//   nome = prompt("insira um nome");
+// }
+
+do {
+  console.log("vc digitou " + nome);
+} while (nome != "");
