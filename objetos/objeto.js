ef32734173564b7f7cd07a9339f53bc32855ed3e
Jean <jeanncavalcante@gmail.com>
2022-11-17 18:03:11 -0300

    att class

--- objetos/objeto.js
+++ objetos/objeto.js
@@ -1,10 +1,43 @@
-const pessoa = {
-  nome: "jean",
-  idade: "75",
-  endereco: {
-    rua: "rua 1",
-    numero: "123",
-  },
+const nome = prompt("Digite seu nome");
+const oQueEstaFazendo = parseInt(
+  prompt(
+    `Digite o que esta fazendo com apenas o numero correspondente: \n 1 - estudando, 2 - comendo, 3 - dormindo`
+  )
+);
+
+class Pessoa {
+  constructor(nome, oQueEstaFazendo) {
+    this.nome = nome;
+    this.oQueEstaFazendo = oQueEstaFazendo;
+  }
+  estudando() {
+    return alert(this.nome + " Esta Estudando");
+  }
+  comendo() {
+    return alert(this.nome + " Esta comendo");
+  }
+  dormindo() {
+    return alert(this.nome + " Esta dormindo");
+  }
+}
+
+function verificaMethod(){
+  const pessoa = new Pessoa(nome, oQueEstaFazendo);
+  switch (oQueEstaFazendo) {
+    case 1:
+      pessoa.estudando();
+      break;
+    case 2:
+      pessoa.comendo();
+      break;
+    case 3:
+      pessoa.dormindo();
+      break;
+    default:
+      alert("Digite um numero valido, ex: 1, 2 ou 3");
+      location.reload();
+  }
 };
 
-console.log(pessoa);
+verificaMethod();
+
